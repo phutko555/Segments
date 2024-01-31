6a5b62d77a7e556762979bf3cc692f66e73be641
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-01-31 00:14:48 +0000

    Update Point.java

--- src/main/java/com/epam/rd/autotasks/segments/Point.java
+++ src/main/java/com/epam/rd/autotasks/segments/Point.java
@@ -1,8 +1,8 @@
 package com.epam.rd.autotasks.segments;
 
 class Point {
-    private double x;
-    private double y;
+    private final double x;
+    private final double y;
 
     public Point(final double x, final double y) {
         this.x = x;
@@ -15,5 +15,24 @@ class Point {
 
     public double getY() {
         return y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Point other = (Point) obj;
+        return Double.compare(other.x, x) == 0 && Double.compare(other.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        long xBits = Double.doubleToLongBits(x);
+        long yBits = Double.doubleToLongBits(y);
+        result = 31 * result + (int) (xBits ^ (xBits >>> 32));
+        result = 31 * result + (int) (yBits ^ (yBits >>> 32));
+        return result;
     }
 }
